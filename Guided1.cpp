c1aaf26d94accdf928aba4a05b433732b8bd04f1
Syalaisha-Nisrina <ichanisrina04@gmail.com>
2024-03-22 09:14:36 +0700

    modul 2

---
+++ Guided1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+ // Program input array 3 Dimensi
+ int main()
+ {
+    int arr [2][3][3];
+    for (int x = 0; x < 2; x++)
+    {
+        for (int y = 0; y < 3; y++)
+        {
+            for (int z = 0; z < 3; z++)
+            {
+                cout << "Input Array ["<< x << "] ["<< y <<"] ["<< z <<"] = ";
+                cin >> arr [x][y][z];
+            }
+        }
+        cout << endl;
+    }
+ 
+// Output array 
+    for (int x = 0 ; x < 2; x++)
+    {
+        for (int y =0; y < 3; y++)
+        {
+            for (int z = 0; z < 3; z++)
+            {
+                cout << "Data Array ["<< x << "] ["<< y <<"] ["<< z <<"] =" << arr [x][y][z] << endl ;
+            }
+        }
+    }
+    cout << endl ;
+    // Tampilan array
+    for (int x = 0 ; x < 2; x++)
+    {
+        for (int y =0; y < 3; y++)
+        {
+            for (int z = 0; z < 3; z++)
+            {
+                cout << arr[x][y][z]  << ends ;
+            }
+            cout << ends;
+        }
+        cout << ends;
+    }
+ }
